04f82365519efba8442425a4cab4f887ee6acb1c
porterorloff <31974361+porterorloff@users.noreply.github.com>
2018-02-08 17:01:09 -0800

    Add files via upload

---
+++ events.py
@@ -0,0 +1,5 @@
+import pygame
+from pygame.locals import *
+class newevent:
+	def __init__(self):
+		return None
--- platformergamePorter.py
+++ platformergamePorter.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import events
 class app:
 	def __init__(self):
 		self._running=True
@@ -13,22 +14,23 @@ class app:
 		self.platform=pygame.image.load("platform.png")
 		self.camera=pygame.image.load("securitycamera.png")
 		self.cannon=pygame.image.load("cannon.png")
+		self.character=pygame.image.load("character.jpg")
 		self.cannon =pygame.transform.rotate(self.cannon,90)
+		self.images=[(self.platform,(400,450)),(self.platform, (100,600)),(self.background, (0,0)),(self.camera,(800,120)),(self.cannon,(110,120))]
 	def on_event(self,event):
 		if event.type==pygame.QUIT:
 			self._running=False
+		if event.type == pygame.KEYDOWN:
+			if event.key == K_SPACE:
+
+
 	def on_loop(self):
 		pass
 
 	def on_render(self):
 		self.screen.fill([255,255,255])
-		self.screen.blit(self.background,(0,0))
-		self.screen.blit(self.platform,(100,600))
-		self.screen.blit(self.platform,(100,600))
-		self.screen.blit(self.platform,(400,400))
-		self.screen.blit(self.camera,(800,120))
-		self.screen.blit(self.cannon,(800,120))
-
+		for coordinate in self.platforms:
+			self.screen.blit(self.platform,coordinate)
 		pygame.display.update()
 
 	def on_cleanup(self):
@@ -44,5 +46,20 @@ class app:
 			self.on_loop()
 			self.on_render()
 		self.on_cleanup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 theApp=app()
 theApp.on_execute() 
