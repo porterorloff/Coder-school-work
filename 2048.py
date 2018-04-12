fde5f5d94cfcd1722ca9d21d10bda3a34e2f0b14
porterorloff <31974361+porterorloff@users.noreply.github.com>
2018-04-12 16:59:14 -0700

    Add files via upload

--- 2048.py
+++ 2048.py
@@ -1,3 +1,4 @@
+import random
 grid=[[""]*4,[""]*4,[""]*4,[""]*4]
 
 
@@ -21,151 +22,102 @@ def is_win(grid):
 
 	return False
 
+def valid_move(grid,move):
+	copy=[row.copy() for row in grid]
+	move(copy)
+	if copy == grid:
+		return False
+	return True
+
+def is_lose(grid):
+
+
 def left(grid):
 	for y in range(4):
+		combined =[False]*4
 		for x in range(4):
 			if grid [y][x]=="":
 				continue
 			pickedup =grid [y][x]
 			grid [y][x]=""
 			for spot in range(x,-2,-1):
-				if grid [y][spot]=="":
-					continue 
 				if spot==-1:
 					grid [y][0]=pickedup
-				if pickedup==grid [y][spot]:
+					break
+				if grid [y][spot]=="":
+					continue 
+				if pickedup==grid [y][spot]and not combined[spot]:
 					grid[y][spot] *=2
+					combined [spot]=True
+					break
 				else:
-					grid [y][spot+1]=pickedup	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+					grid [y][spot+1]=pickedup
+					break
+
+def right(grid):
+	for y in range(4):
+		combined =[False]*4
+		for x in range(3,-1,-1):
+			if grid[y][x]=="":
+				continue
+			pickedup =grid [y][x]
+			grid [y][x]=""
+			for spot in range(x,5,1):
+				if spot==4:
+					grid [y][3]=pickedup
+					break
+				if grid [y][spot]=="":
+					continue 
+				if pickedup==grid [y][spot] and not combined[spot]:
+					grid[y][spot] *=2
+					combined [spot]=True
+					break
+				else:
+					grid [y][spot-1]=pickedup
+					break
+
+
+def up(grid):
+	for x in range(4):
+		combined =[False]*4
+		for y in range(4):
+			if grid [y][x]=="":
+				continue
+			pickedup =grid [y][x]
+			grid [y][x]=""
+			for spot in range(y,-2,-1):
+				if spot==-1:
+					grid [0][x]=pickedup
+					break
+				if grid [spot][x]=="":
+					continue 
+				if pickedup==grid [spot][x] and not combined[spot]:
+					grid[spot][x] *=2
+					combined [spot]=True
+					break
+				else:
+					grid [spot+1][x]=pickedup
+					break
+
+
+def down(grid):
+	for x in range(4):
+		combined =[False]*4
+		for y in range(3,-1,-1):
+			if grid [y][x]=="":
+				continue
+			pickedup =grid [y][x]
+			grid [y][x]=""
+			for spot in range(y,5,1):
+				if spot==4:
+					grid [3][x]=pickedup
+					break
+				if grid [spot][x]=="":
+					continue 
+				if pickedup==grid [spot][x] and not combined[spot]:
+					grid[spot][x] *=2
+					combined [spot]=True
+					break
+				else:
+					grid [spot-1][x]=pickedup
+					break
